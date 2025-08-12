19420bbae1a65d9201ff9eb3f264f9add0f20885
rahul-4321m <shahrahul2051@gmail.com>
2025-08-12 23:51:56 +0530

    added delete todo

--- Todo-list/main.go
+++ Todo-list/main.go
@@ -170,3 +170,26 @@ func createTodo(w http.ReqestResponse, r* http.Request){
 		"todo_id":tm.ID.Hex(),
 	})
 }
+
+
+func deleteTodo(w httpResponseWriter,r * http.Request){
+	id:=strings.TrimSpace(chi.URLParam(r,"id"))
+	
+	if !bson.IsObjectIdHex(id){
+		rnd.JSON(w,http.StatusBadRequst,renderer.M{
+			"message":"The id is invalid",
+		})
+		return
+	}
+
+	if err:=db.C(collectionName).RemoveId(bson.ObjectIdHex(id));err!=nil{
+		rnd.JSON(w,http.StatusProcessing,renderer.M{
+			"message":"Failed to delete todo",
+		})
+		return
+	}
+
+	rnd.JSON(w,http.StatusOK,renderer.M{
+		"message":"Todo deleted successfully",
+	})
+}
